4273aa13f57c38730b42907df87bffe890fa152b
KamarulAdha <kamarul.adha360@gmail.com>
2020-07-10 07:07:07 +0800

    Updated Static Path

--- main/settings.py
+++ main/settings.py
@@ -122,11 +122,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-    # '/static/',
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+#     # '/static/',
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
