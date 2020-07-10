4b6f021cc05a0b6580e9b66bb3e9a868bc033ec3
KamarulAdha <kamarul.adha360@gmail.com>
2020-07-10 10:04:27 +0800

    Update

--- main/urls.py
+++ main/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('53cr37_4dm1n_1o6in_94g3_h3h3/', admin.site.urls),
     path('api/', include('element.urls')),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
