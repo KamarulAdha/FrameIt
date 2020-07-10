c754d06796a4ecc82c1f89ff394ccba39a062dad
KamarulAdha <kamarul.adha360@gmail.com>
2020-07-10 23:15:44 +0800

    Debug Status

--- main/settings.py
+++ main/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e45h1ytf7asf+!f)xba6oz#0q#79$xxlz9s5vj9cgbo^vt*-^v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['frameitdjangorestapi.herokuapp.com',]
 
